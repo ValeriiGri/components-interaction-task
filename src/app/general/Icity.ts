b117b0fd70d9db572d75ceb8a23fc22d7c34b447
root <valeragg65@gmail.com>
2020-02-09 23:13:11 +0300

    - directives & pipes

---
+++ src/app/general/Icity.ts
@@ -0,0 +1,8 @@
+export interface ICity {
+    imgPath1: string;
+    imgPath2: string;
+    iconPath1: string;
+    iconPath2: string;
+    title: string;
+    }
+
--- src/app/general/general.component.ts
+++ src/app/general/general.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, Output, EventEmitter} from '@angular/core';
-
+import {ICity} from './Icity';
 
 @Component({
   selector: 'task-general',
@@ -8,54 +8,64 @@ import {Component, OnInit, Output, EventEmitter} from '@angular/core';
 })
 export class GeneralComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
 
-  public img1 = './assets/imgs/ny.jpg';
-  public img2 = './assets/imgs/sea.jpg';
-  public img3 = './assets/imgs/wf.jpg';
-  public img4 = './assets/imgs/hd.jpg';
-  public img5 = './assets/imgs/init1.jpg';
-  public img6 = './assets/imgs/init2.jpg';
+  }
+
+
+  public cities: Array<ICity> = [ { imgPath1: './assets/imgs/ny.jpg', imgPath2: './assets/imgs/sea.jpg', title: 'New-York',
+                                iconPath1: './assets/imgs/ny_icon.jpg', iconPath2: './assets/imgs/sea_icon.jpg'},
+                              {imgPath1: './assets/imgs/wf.jpg', imgPath2: './assets/imgs/hd.jpg', title: 'Paris',
+                                iconPath1: './assets/imgs/wf_icon.jpg', iconPath2: './assets/imgs/hd_icon.jpg'},
+                            ];
+/*{img5Path: './assets/imgs/init1.jpg'},
+{img6Path: './assets/imgs/init2.jpg'}*/
+
+
   public imgFromBtn2: string;
-  public imgFromBtn: string = this.img5;
+  public imgFromBtn: string = this.cities[0].imgPath1;
 
   @Output()
   public setGeneralControl: EventEmitter<string> = new EventEmitter<string>();
 
 
-  public imgToHead(flag: number): void {
+  public imgToHeadAndSocialComp(flag: number): void {
     switch (flag) {
       case 0:
-        this.imgFromBtn = this.img1;
+        this.imgFromBtn = this.cities[flag].imgPath1;
+        this.imgFromBtn2 = this.cities[flag].imgPath2;
+        //this.setGeneralControl.emit(this.imgFromBtn2);
         break;
       case 1:
-        this.imgFromBtn = this.img3;
+        this.imgFromBtn = this.cities[flag].imgPath1;
+        this.imgFromBtn2 = this.cities[flag].imgPath2;
+        this.setGeneralControl.emit(this.imgFromBtn2);
         break;
     }
   }
 
-  public imgToSocialComp(flag: number):void {
+  /*public imgToSocialComp(flag: number):void {
     switch (flag) {
       case 0:
-        this.imgFromBtn2 = this.img2;
+        this.imgFromBtn2 = this.cities[4].img5Path;
         this.setGeneralControl.emit(this.imgFromBtn2);
         break;
       case 1:
-        this.imgFromBtn2 = this.img4;
+        this.imgFromBtn2 = this.cities[3].img4Path;
         this.setGeneralControl.emit(this.imgFromBtn2);
         break;
       case 2:
-        this.imgFromBtn2 = this.img6;
+        this.imgFromBtn2 = this.cities[5].img6Path;
         this.setGeneralControl.emit(this.imgFromBtn2);
         break;
     }
 
-  }
+  }*/
 
 
 
   ngOnInit() {
-    this.imgToSocialComp(2);
+    this.imgToHeadAndSocialComp(0);
   }
 
 }
